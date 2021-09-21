dc9d74d6e482a8550a3b629c2753bc6d5af499e6
shuvam kumar <shuvam1982000@gmail.com>
2021-09-21 19:59:53 +0530

    login route added

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import AllProject from '../views/AllProject.vue'
+import Login from '../views/Login.vue'
 
 const routes = [{
   path: '/',
@@ -11,8 +12,12 @@ const routes = [{
   name: 'Project',
   component: AllProject
 }, {
-  path : '/add-projects',
+  path: '/add-projects',
   name: 'Add-Project'
+}, {
+  path: '/login',
+  name: 'Login',
+  component: Login,
 }
 ]
 
---
+++ src/views/Login.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="login">
+    <input type="email" class="input-field login-email-id" />
+    <input type="password" class="input-field login-password" />
+    <button class="login-button">Login</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+};
+</script>
+
+<style scoped>
+.login {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.input-field {
+  border: none;
+  border-radius: 50px;
+  border: #b0bbda 1px solid;
+  background: transparent;
+  resize: none;
+  outline: none;
+  margin: 10px;
+  padding: 10px;
+  width: 300px;
+  height: 30px;
+  font-size: 20px;
+  text-align: center;
+  color: #b0bbda;
+}
+
+.login-password {
+  font-size: 30px;
+}
+
+.login-button {
+  border: none;
+  outline: none;
+  margin: 30px;
+  padding: 15px;
+  width: 200px;
+  border-radius: 50px;
+  background: transparent;
+  color: #64ffda;
+  border: #64ffda 1px solid;
+}
+</style>
